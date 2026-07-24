91775c275cd52bae1335ca5f3ee42d20abcf570f
kevinuyyy <kevinabigail2201@gmail.com>
2026-07-24 07:50:39 +0700

    Add files via upload

---
+++ kevin.js
@@ -0,0 +1,4 @@
+console.log("haloo nama saya ivandya kevin abigail jeconia")
+console.log("saya lahir 22 january 2011")
+console.log("saya dari MTsN 2 KOTA MALANG")
+console.log("alasan saya ingin masuk smk telkom malang ini bukan hanya belajar coding tapi juga ingin mencapai salah satu keinginan saya yaitu kuliah/bekerja di luar negeri")
