f57e8f1a99c8f7f1f48eb7eda79105d69ddf3fdf
kevinuyyy <kevinabigail2201@gmail.com>
2026-07-31 08:00:21 +0700

    Add files via upload

---
+++ karyawan.js
@@ -0,0 +1,15 @@
+// string
+let nama = "ivandya kevin abigail jeconia"
+//number
+let umur = 17;
+//number
+let gaji = 1000000000;
+//boolean
+let statuskartap = true;
+//null
+let jabatan = "ceo";
+console.log(`nama karyawan: ${nama}`);
+console.log(`umur karyawan: ${umur}`);
+console.log(`gaji karyawan: ${gaji}`);
+console.log(`status kartu pegawai: ${statuskartap}`);
+console.log(`jabatan karyawan: ${jabatan}`);
